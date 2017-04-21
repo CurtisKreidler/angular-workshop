218ce7977f642a973270b542a6fe7bf6bcb8e701
CurtisKreidler <kreidler@udel.edu>
2017-04-21 04:38:29 -0400

    adding removed blank project back

---
+++ angular2-todo/src/app/task-data.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.TodoList = [
+    { isDone: false, itemName: "feed the dog" },
+    { isDone: false, itemName: "finish workshop" },
+    { isDone: false, itemName: "be cool" },
+    { isDone: false, itemName: "ignore finance homework" },
+    { isDone: false, itemName: "buy groceries" },
+    { isDone: false, itemName: "study" }
+];
+//# sourceMappingURL=task-data.js.map
---
+++ angular2-todo/src/app/task.service.ts
@@ -0,0 +1,7 @@
+import { TodoList } from './task-data';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class TaskService {
+
+}
